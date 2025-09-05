cf1fb964cadcde09b6f45bfad9affa1542254368
Loreto Bravo <lbravoc@users.noreply.github.com>
2025-09-05 10:26:00 -0400

    PWeb

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/mujeresSTEM/', // 👈 importante
+  // para que el build vaya directo a /docs (lo que usa GitHub Pages)
+  build: { outDir: 'docs' },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
